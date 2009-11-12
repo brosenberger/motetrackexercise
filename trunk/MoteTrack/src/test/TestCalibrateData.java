3e7c70bfddd10913060c9de05a8a412b57ab2943
benjamin.rosenberger@liwest.at <benjamin.rosenberger@liwest.at@634393fc-cd2f-11de-aad3-235a7848f092>
2009-11-12 16:54:24 +0000

    Normalization, filters some data values

---
+++ trunk/MoteTrack/src/test/TestCalibrateData.java
@@ -0,0 +1,20 @@
+package test;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import misc.DataCalibrator;
+import misc.ServerLogger;
+import misc.TestObserver;
+import network.ServerDataReader;
+
+public class TestCalibrateData {
+	public static void main(String[] args) {
+		System.out.println("Starting client");
+		DataCalibrator calib = new DataCalibrator(70);
+		calib.addObserver(new TestObserver());
+		ServerDataReader reader = new ServerDataReader("localhost",5000,"020000136187");
+		reader.addObserver(calib);
+		reader.startReader();
+	}
+}
