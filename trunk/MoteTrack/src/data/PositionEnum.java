1abd3b37995ba7d376ee84657bae04ce924a80ee
thomas@scheinecker.at <thomas@scheinecker.at@634393fc-cd2f-11de-aad3-235a7848f092>
2009-12-03 13:36:51 +0000

    Position Enum

---
+++ trunk/MoteTrack/src/data/PositionEnum.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package data;
+
+/**
+ *
+ * @author tscheinecker
+ */
+public enum PositionEnum {
+
+    leftShoulder,
+    rightShoulder,
+    leftWrist,
+    rightWrist,
+    hip,
+    leftAnkle,
+    rightAnkle
+}
